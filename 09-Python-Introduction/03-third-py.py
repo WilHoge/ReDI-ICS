5b0e7ce00cc52ae8a4bdf19dbcd3b49ad89411cd
Wilfried Hoge <wilfried@hoge-uebler.de>
2022-04-02 12:45:47 +0200

    Initial version

---
+++ 09-Python-Introduction/03-third-py.py
@@ -0,0 +1,9 @@
+redi_class = [
+    {'name': 'Mohamed Abadi', 'age': 23},
+    {'name': 'Joseph Otieno', 'age': 28},
+    {'name': 'Joaninha Carneirois', 'age': 21},
+    {'name': 'Sophie Huber', 'age': 24}
+]
+
+for student in redi_class:
+          print(f"Hello {student['name']}")
