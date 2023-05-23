dcf53337de2c0424bd066834c154d70399131896
WilHoge <WilHoge@users.noreply.github.com>
2023-05-23 10:30:53 +0000

    changes for replit

--- 21-Flask/1-hello-world/hello-user.py
+++ 21-Flask/1-hello-world/hello-user.py
@@ -1,13 +1,17 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+  return 'Hello World!'
 
-@app.route('/user/<name>') 
+
+@app.route('/user/<name>')
 def user(name):
   return '<h1>Hello, {}!</h1>'.format(name)
 
+
 if __name__ == '__main__':
-    app.run()
+  app.run(host='0.0.0.0', port=81)
--- 21-Flask/4-form/hello-form.py
+++ 21-Flask/4-form/hello-form.py
@@ -1,25 +1,29 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'this is a secret'
 
-from flask_wtf import FlaskForm 
-from wtforms import StringField, SubmitField 
-from wtforms.validators import DataRequired 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
-class NameForm(FlaskForm): 
-    name = StringField('What is your name?', validators=[DataRequired()]) 
-    submit = SubmitField('Submit')
+
+class NameForm(FlaskForm):
+  name = StringField('What is your name?', validators=[DataRequired()])
+  submit = SubmitField('Submit')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
-    form = NameForm()
-    if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+  name = None
+  form = NameForm()
+  if form.validate_on_submit():
+    name = form.name.data
+    form.name.data = ''
+  return render_template('index.html', form=form, name=name)
+
 
 if __name__ == '__main__':
-    app.run()
+  app.run(host='0.0.0.0', port=81)
